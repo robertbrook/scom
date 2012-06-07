32a8cb297eb09525ce6fa24c245027cbdfc1c979
Robert Brook <mail@robertbrook.com>
2012-06-07 12:16:47 +0100

    trying mkpath

--- reaper.rb
+++ reaper.rb
@@ -1,24 +1,33 @@
 require 'rubygems'
 require 'nokogiri'
-require 'open-uri'
-require 'uri'
+require 'logger'
+require 'fileutils'
+
+@log = Logger.new(STDOUT)
+@log.level = Logger::DEBUG
+@log.formatter = proc do |severity, datetime, progname, msg|
+        "#{msg}\n"
+end
+
 
 class MyDocument < Nokogiri::XML::SAX::Document
 
 def initialize
-    @chunks = ""
+        @chunks = ""
 end
 
 attr_reader :chunks
 
 def end_document
-	# puts "the document has ended"
+	""
 end
 
 def start_element name, attributes = []
         case name
-        when 'p','i','b','table','tr','td','ol','li','section'
+        when 'p','i','b','tr','td','ol','li','section'
                 @chunks <<  "\n\t<#{name}>"
+        when 'table'
+                @chunks <<  "\t<table class='table table-striped'>"
         when 'date'
                 @chunks <<  "\t<time>"
         when 'lb'
@@ -30,7 +39,7 @@ def start_element name, attributes = []
         when 'image'
                 @chunks << '<span class="label imageref" title="' << attributes[0][1] << '">JPEG</span>'
         when 'frontpage'
-                @chunks <<  "\n\t<div class='frontpage well'>"
+                @chunks <<  "\n\t<div class='frontpage'>"
         else
                 @chunks <<  "\n\t<div class='#{name}'>"
         end
@@ -69,58 +78,62 @@ end
 
 end
 
-IO.readlines('urls.txt').each do |url|
-        
-        uri = URI(url) 
-        myfilename = File.basename(uri.path, ".xml")
+Dir.glob('./input/*.xml') do |xml_file|
+        myfilename = File.basename(xml_file, ".xml")
         myfile = File.new("./output/" + myfilename + ".html", "w")
+        # FileUtils.mkpath("./output/#{myfilename.split("-")[0]}/#{myfilename.split("-")[1]}/")
+        
         myDoc = MyDocument.new
         parser = Nokogiri::XML::SAX::Parser.new(myDoc)
+        parser.parse(open(xml_file))
+        
+        @log.info("Parsed #{xml_file}")
 
-        parser.parse(open(url))
-        puts "Parsing #{url}"
         header = <<END
-        <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en-GB">
-<head>
-	<meta charset="utf-8">
-	<title>#{myfile}</title>
-	 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	    <link href="http://robertbrook.com/css/bootstrap.min.css" rel="stylesheet">
-<link href="http://robertbrook.com/css/bootstrap-responsive.min.css" rel="stylesheet">
-<!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-    
-    <style>
-    body {padding:1em 0;}
-    div.member {display:inline;font-weight:bold;}
-    div.membercontribution {display:inline;}
-    span.column {float:right;margin-left:0.5em;}
-    span.imageref {margin-left:0.5em;float:right;}
-    </style>
-</head>
-<body>
+        <head>
+                <meta charset="utf-8">
+                <title>#{myfilename}</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link href="../css/bootstrap.min.css" rel="stylesheet">
+                <link href="../css/bootstrap-responsive.min.css" rel="stylesheet">
+                <!--[if lt IE 9]>
+                <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+                <![endif]-->
+                <style>
+                body {padding:1em 0;}
+                div.bill {font-weight:bold;font-size:larger;text-align:center;}
+                div.member {display:inline;font-weight:bold;}
+                div.memberconstituency {display:inline;font-weight:bold;}
+                div.membercontribution {display:inline;}
+                span.column {float:right;margin-left:0.5em;}
+                span.imageref {margin-left:0.5em;float:right;}
+                div.frontpage {border:1pt dotted gray;padding:20px;margin-top:10px;margin-bottom:10px;}
+                </style>
+        </head>
+        <body>
 
-<div class="container">
-      <div class="row">
-
-<div class="span8 offset1">
+        <div class="container">
+                <div class="row">
+                        <div class="span8 offset1">
 END
 
         myfile.write header
         myfile.write myDoc.chunks
         footer = <<END
-        </div>
-
 </div>
 </div>
 </div>
-
+</div>
+    <script src="../js/jquery-1.7.2.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
 </body>
 </html>
 END
         myfile.write footer
+        @log.info("Written #{myfilename}")
+        
 
        
 end
